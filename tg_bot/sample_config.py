ca581ef6c99e45bd2057e61571dc7dd351309e9e
DarkWeb <54339318+TheDarkW3b@users.noreply.github.com>
2020-01-18 14:12:37 +0530

    Update sample_config.py

--- tg_bot/sample_config.py
+++ tg_bot/sample_config.py
@@ -11,8 +11,8 @@ class Config(object):
 
     # REQUIRED
     API_KEY = ""
-    OWNER_ID = "683538773" # If you dont know, run the bot and do /id in your private chat with it
-    OWNER_USERNAME = "ViruZs"
+    OWNER_ID = "686021814" # If you dont know, run the bot and do /id in your private chat with it
+    OWNER_USERNAME = "TheDarkW3b"
 
     # RECOMMENDED
     SQLALCHEMY_DATABASE_URI = 'sqldbtype://username:pw@hostname:port/db_name'  # needed for any database modules
@@ -32,7 +32,7 @@ class Config(object):
     DEL_CMDS = False  # Whether or not you should delete "blue text must click" commands
     STRICT_GBAN = False
     WORKERS = 8  # Number of subthreads to use. This is the recommended amount - see for yourself what works best!
-    BAN_STICKER = 'CAADAgADOwADPPEcAXkko5EB3YGYAg'  # banhammer marie sticker
+    BAN_STICKER = 'CAADBQADfgADttzjKJWTC3CM9l78FgQ'  # banhammer marie sticker
     ALLOW_EXCL = False  # Allow ! commands as well as /
 
 
