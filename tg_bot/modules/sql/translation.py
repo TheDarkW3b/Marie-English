0677f86592c3239feaaa31948d51edbaa55de508
DarkWeb <54339318+TheDarkW3b@users.noreply.github.com>
2020-01-18 23:07:10 +0530

    Introducing Feds

--- tg_bot/modules/sql/translation.py
+++ tg_bot/modules/sql/translation.py
@@ -1,6 +1,6 @@
 import threading
 from sqlalchemy import Column, String, UnicodeText
-from menhera.modules.sql import SESSION, BASE
+from tg_bot.modules.sql import SESSION, BASE
 
 class Locales(BASE):
     __tablename__ = "Locales"
