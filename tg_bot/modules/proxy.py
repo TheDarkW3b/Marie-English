241db114f1006d4e948334d2fb5c69e603ce0b7d
DarkWeb <54339318+TheDarkW3b@users.noreply.github.com>
2020-02-11 13:34:31 +0530

    Update proxy.py

--- tg_bot/modules/proxy.py
+++ tg_bot/modules/proxy.py
@@ -1,5 +1,25 @@
-from telegram import ChatActionimport htmlimport urllib.requestimport reimport jsonfrom typing import Optional, Listimport timeimport urllibfrom urllib.request import urlopen, urlretrievefrom urllib.parse import quote_plus, urlencodeimport requestsfrom telegram import Message, Chat, Update, Bot, MessageEntityfrom telegram import ParseModefrom telegram.ext import CommandHandler, run_async, Filtersfrom tg_bot.modules.helper_funcs.filters import CustomFiltersfrom tg_bot import dispatcherfrom tg_bot.__main__ import STATS, USER_INFO
-from tg_bot.modules.translations.strings import tld def proxy(bot: Bot, update: Update): proxy = requests.get('https://proxy11.com/api/proxy.json?key=OTA4.XkBkMw.kXAnDeXngEOrMlYcQ65ueOewBCY').json()[0]["preview"] final = "https://proxy11.com/{}".format(proxy) update.message.reply_text(tld(final)
+from telegram import ChatAction
+import html
+import urllib.request
+import reimport json
+from typing import Optional, List
+import time
+import urllib
+from urllib.request import urlopen, urlretrieve
+from urllib.parse import quote_plus, urlencode
+import requests
+from telegram import Message, Chat, Update, Bot, MessageEntity
+from telegram import ParseMode
+from telegram.ext import CommandHandler, run_async, Filters
+from tg_bot.modules.helper_funcs.filters import CustomFilters
+from tg_bot import dispatcher
+from tg_bot.__main__ import STATS, USER_INFO
+from tg_bot.modules.translations.strings import tld
+
+@run_async
+def proxy(bot: Bot, update: Update): 
+	proxy = requests.get('https://proxy11.com/api/proxy.json?key=OTA4.XkBkMw.kXAnDeXngEOrMlYcQ65ueOewBCY').json()[0]["preview"] 
+	final = "https://proxy11.com/{}".format(proxy) update.message.reply_text(tld(final)
 return ""	
 
 __help__ = """ - * Only For Sudos Now * - /proxy: Get Updated Proxy list"""
